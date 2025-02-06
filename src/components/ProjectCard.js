43e0c15deb240bc8c447eb143cb8f10b27d16528
narminfg <narminfg@code.edu.az>
2025-02-06 13:32:51 +0400

    some changes

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,12 @@
-import { Col } from "react-bootstrap";
+import React from "react";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ imgUrl, description, url }) => {
   return (
-    <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
+    <div className="proj-imgbx" onClick={() => window.open(url, "_blank")}>
+      <img src={imgUrl} alt={description} />
+      <div className="proj-txtx">
+        <h4>{description}</h4>
       </div>
-    </Col>
-  )
-}
+    </div>
+  );
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,25 +4,26 @@ import projImg1 from "../assets/img/pronia.png";
 import projImg2 from "../assets/img/rezervation.png";
 import projImg3 from "../assets/img/todolist.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Projects = () => {
-
   const projects = [
     {
-      description: "Design & Development",
+      description: "Pronia",
       imgUrl: projImg1,
+      url: "https://github.com/narminfg/Pronia_FinalProject_Back",
     },
     {
-      description: "Design & Development",
+      description: "Rezervation",
       imgUrl: projImg2,
+      url:"https://github.com/narminfg/rezervation"
     },
     {
-      description: "Design & Development",
+      description: "Todolist",
       imgUrl: projImg3,
+      url:"https://github.com/narminfg/ToDoList-with-react_redux"
     },
-  
   ];
 
   return (
@@ -31,55 +32,75 @@ export const Projects = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Below are some of the projects I’ve worked on, demonstrating my skills in frontend development, problem-solving, and creating user-centered web applications</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    Below are some of the projects I’ve worked on, demonstrating
+                    my skills in frontend development, problem-solving, and
+                    creating user-centered web applications
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
                             return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>Pronia:<br/>"I use HTML, CSS, Bootstrap, and JavaScript for the frontend, .NET Core for the backend, and MSSQL for the database in the Pronia website."</p>
-                      <p>Rezervation: <br/> "I built this project using React and Redux Toolkit. For forms, I used React Hook Form, and for the UI, I utilized Material-UI."</p>
-                      <p>TodoList: <br/>"I built this project using React and Redux Toolkit."</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>"Pronia" github: https://github.com/narminfg/Pronia_FinalProject_Back</p>
-                      <p>"Rezervation" github : https://github.com/narminfg/rezervation</p>
-                      <p>"TodoList" github : https://github.com/narminfg/ToDoList-with-react_redux</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                              <Col key={index} md={4} sm={12}>
+                                <ProjectCard {...project} />
+                              </Col>
+                            );
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <p>
+                          Pronia:
+                          <br />
+                          "I use HTML, CSS, Bootstrap, and JavaScript for the
+                          frontend, .NET Core for the backend, and MSSQL for the
+                          database in the Pronia website."
+                        </p>
+                        <p>
+                          Rezervation: <br /> "I built this project using React
+                          and Redux Toolkit. For forms, I used React Hook Form,
+                          and for the UI, I utilized Material-UI."
+                        </p>
+                        <p>
+                          TodoList: <br />
+                          "I built this project using React and Redux Toolkit."
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+};
